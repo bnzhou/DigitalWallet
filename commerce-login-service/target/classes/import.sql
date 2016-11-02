21edab32145f5795043a24e7dbf9ffb2071e6a7a
Bingnan Zhou <bnzhou@cmu.edu>
2016-11-02 19:51:52 -0400

    update login service

---
+++ commerce-login-service/target/classes/import.sql
@@ -0,0 +1,2 @@
+insert into login (username, password, description) values ('user1', 'user1', 'customer1');
+insert into login (username, password, description) values ('user2', 'user2', 'customer2');
